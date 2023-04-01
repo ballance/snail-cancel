7f7803f2ce6e48b1925ae0d0cd1a782bb92b4c88
ballance <ballance@gmail.com>
2023-04-01 12:36:04 -0400

    updating gitignore
    updating gradle config for api project

--- src/api/src/main/java/it/ballance/snail/SnailApplication.java
+++ src/api/src/main/java/it/ballance/snail/SnailApplication.java
@@ -2,8 +2,9 @@ package it.ballance.snail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SnailApplication {
 
     public static void main(String[] args) {

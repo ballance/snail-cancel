7e03cca5b328d8aa63fc77c273255f073c2a4c67
ballance <ballance@gmail.com>
2023-04-01 10:28:49 -0400

    Landing page for FE

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import snailLogo from './assets/images/snail.png'
 import './App.css'
 
 function App() {
@@ -9,24 +8,11 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <img src={snailLogo} className="logo" alt="Snail Logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Snail Cancel</h1>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Lorem Ipsum sit dolor amet avec qua.
       </p>
     </div>
   )
